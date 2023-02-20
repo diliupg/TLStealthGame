232097e8bebf30eeae86c54a62b456ee1c1ccaff
diliupg <unitygamecodersl@gmail.com>
2023-02-20 23:59:12 +0530

    S04 L08 Add 3D UI State above Guard

--- Source/FPSGame/Public/FPSAIGuard.h
+++ Source/FPSGame/Public/FPSAIGuard.h
@@ -8,6 +8,16 @@
 
 class UPawnSensingComponent;
 
+UENUM(BlueprintType )
+enum class EAIState : uint8
+{
+	Idle,
+
+	Suspicious,
+
+	Alerted
+};
+
 UCLASS()
 class FPSGAME_API AFPSAIGuard : public ACharacter
 {
@@ -37,6 +47,13 @@ protected:
 
 	FTimerHandle FTH_ResetOrientation;
 
+	EAIState GuardState;
+
+	void SetGuardState( EAIState NewState );
+
+	UFUNCTION(BlueprintImplementableEvent, Category = AI )
+	void OnStateChanged( EAIState NewState );
+	 
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
